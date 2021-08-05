c8f3d0ab7b8cef5e2d3455baaecb4320e5c181f3
Esteban <cehernandezco@gmail.com>
2021-08-06 00:53:56 +1000

    first commit

---
+++ cocktailAdvisor/CocktailsTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  CocktailsTableViewController.h
+//  cocktailAdvisor
+//
+//  Created by Esteban Hernandez on 5/8/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CocktailsTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
